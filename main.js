f20c2a310de21038bfa0914818f388f155f67670
Taiwo Tejuosho <taiwotejuosho@Taiwos-MBP.hsd1.il.comcast.net>
2017-10-19 22:41:01 -0500

    1.4

--- main.js
+++ main.js
@@ -11,13 +11,15 @@ console.log("--------------------------------------------------------"+"\n");
 var wordArray = ["Tesla", "Mercedes", "Lexus", "Toyota", "Ferrari", "Chrysler", "Maybach", "Volkswagen"]
 
 
+start();
+
 
 function start(){ 
 
 var tesla = ["t", "e", "s", "l", "a"];
 var displayedWord = ["t", "e", "_", "_", "_"];
-console.log(displayedWord);
 var initialWord = ["t", "e"]
+console.log(displayedWord);
 
 
 inquirer.prompt([
@@ -60,10 +62,57 @@ console.log("\n")
 console.log("**************NEXT WORD******************");
 console.log("-----------------------------------------" + "\n")
 
+var toyota = ["t", "o", "y", "o", "t", "a"];
+var displayedWord = ["t", "o", "y", "_", "_", "_"];
+var initialWord = ["t", "o", "y"];
+console.log(displayedWord);
+
+
+inquirer.prompt([
+	{
+		type:"input",
+		name:"guessedLetters1",
+		message:"Choose a letter",
+
+	},{
+		type:"input",
+		name:"guessedLetters2",
+		message:"Choose another letter",
+	},{
+		type:"input",
+		name:"guessedLetters3",
+		message:"Choose Another One",
+	}
+	]).then(function game (answer){
+		
+		initialWord.push(answer.guessedLetters1,answer.guessedLetters2,answer.guessedLetters3);
+
+		// var finalWord = initialWord.concat(guessedLetters);
+
+		if (JSON.stringify(initialWord) == JSON.stringify(toyota)){
+			console.log("\n" + "The Word is " + wordArray[3] + ", Good Job");
+		} else {
+			console.log("\n" + "You Guessed Wrong" + "\n");
+		}
+
+		thirdWord();
+	
+	})
+
+}
+
+
+
+function thirdWord (){
+
+console.log("\n")
+console.log("**************NEXT WORD******************");
+console.log("-----------------------------------------" + "\n")
+
 var mercedes = ["m", "e", "r", "c", "e", "d", "e", "s"];
 var displayedWord = ["m", "e", "r", "c", "_", "_", "_", "_"];
-console.log(displayedWord);
 var initialWord = ["m", "e", "r", "c"];
+console.log(displayedWord);
 
 
 inquirer.prompt([
@@ -92,15 +141,12 @@ inquirer.prompt([
 		// var finalWord = initialWord.concat(guessedLetters);
 
 		if (JSON.stringify(initialWord) == JSON.stringify(mercedes)){
-			console.log("The Word is " + wordArray[1] + ", Good Job");
+			console.log("\n" + "The Word is " + wordArray[1] + ", Good Job");
 		} else {
-			console.log("You Guessed Wrong" + "\n");
+			console.log("\n" + "You Guessed Wrong" + "\n");
 		}
 	
 	})
 
 }
-
-start();
-
 
