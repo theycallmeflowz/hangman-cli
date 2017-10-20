d6f5c645282db4cfbcefaf0a38a83e07c182edb3
Taiwo Tejuosho <taiwotejuosho@Taiwos-MBP.hsd1.il.comcast.net>
2017-10-19 23:00:37 -0500

    1.5

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ console.log("--------------------------------------------------------"+"\n");
 
 
 var wordArray = ["Tesla", "Mercedes", "Lexus", "Toyota", "Ferrari", "Chrysler", "Maybach", "Volkswagen"]
-
+var wins = 0;
+var losses = 0;
 
 start();
 
 
 function start(){ 
 
-var tesla = ["t", "e", "s", "l", "a"];
+var carname = ["t", "e", "s", "l", "a"];
 var displayedWord = ["t", "e", "_", "_", "_"];
 var initialWord = ["t", "e"]
 console.log(displayedWord);
@@ -43,12 +44,16 @@ inquirer.prompt([
 
 		// var finalWord = initialWord.concat(guessedLetters);
 
-		if (JSON.stringify(initialWord) == JSON.stringify(tesla)){
+		if (JSON.stringify(initialWord) == JSON.stringify(carname)){
 			console.log("The Word is " + wordArray[0] + ", Good Job");
+			wins++;
 		} else {
 			console.log("You Guessed Wrong" + "\n");
+			losses++;
 		}
 
+		console.log("\n" + "wins: " + wins);
+		console.log("\n" + "losses: " + losses);
 		secondWord();
 	
 	})
@@ -62,7 +67,7 @@ console.log("\n")
 console.log("**************NEXT WORD******************");
 console.log("-----------------------------------------" + "\n")
 
-var toyota = ["t", "o", "y", "o", "t", "a"];
+var carname = ["t", "o", "y", "o", "t", "a"];
 var displayedWord = ["t", "o", "y", "_", "_", "_"];
 var initialWord = ["t", "o", "y"];
 console.log(displayedWord);
@@ -89,12 +94,16 @@ inquirer.prompt([
 
 		// var finalWord = initialWord.concat(guessedLetters);
 
-		if (JSON.stringify(initialWord) == JSON.stringify(toyota)){
+		if (JSON.stringify(initialWord) == JSON.stringify(carname)){
 			console.log("\n" + "The Word is " + wordArray[3] + ", Good Job");
+			wins++;
 		} else {
 			console.log("\n" + "You Guessed Wrong" + "\n");
+			losses++;
 		}
 
+		console.log("\n" + "wins: " + wins);
+		console.log("\n" + "losses: " + losses);
 		thirdWord();
 	
 	})
@@ -109,7 +118,7 @@ console.log("\n")
 console.log("**************NEXT WORD******************");
 console.log("-----------------------------------------" + "\n")
 
-var mercedes = ["m", "e", "r", "c", "e", "d", "e", "s"];
+var carname = ["m", "e", "r", "c", "e", "d", "e", "s"];
 var displayedWord = ["m", "e", "r", "c", "_", "_", "_", "_"];
 var initialWord = ["m", "e", "r", "c"];
 console.log(displayedWord);
@@ -140,13 +149,68 @@ inquirer.prompt([
 
 		// var finalWord = initialWord.concat(guessedLetters);
 
-		if (JSON.stringify(initialWord) == JSON.stringify(mercedes)){
+		if (JSON.stringify(initialWord) == JSON.stringify(carname)){
 			console.log("\n" + "The Word is " + wordArray[1] + ", Good Job");
+			wins++;
 		} else {
 			console.log("\n" + "You Guessed Wrong" + "\n");
+			losses++
 		}
-	
+
+		console.log("\n" + "wins: " + wins);
+		console.log("\n" + "losses: " + losses);
+		forthWord();
 	})
 
 }
 
+
+function forthWord (){
+
+console.log("\n")
+console.log("**************NEXT WORD******************");
+console.log("-----------------------------------------" + "\n")
+
+var carname = ["v", "o", "l", "k", "s", "w", "a", "g", "e", "n"];
+var displayedWord = ["v", "o", "l", "k", "s", "w", "_", "_", "_", "_"];
+var initialWord = ["v", "o", "l", "k", "s", "w"];
+console.log(displayedWord);
+
+
+inquirer.prompt([
+	{
+		type:"input",
+		name:"guessedLetters1",
+		message:"Choose a letter",
+
+	},{
+		type:"input",
+		name:"guessedLetters2",
+		message:"Choose another letter",
+	},{
+		type:"input",
+		name:"guessedLetters3",
+		message:"Choose Another One",
+	},{
+		type:"input",
+		name:"guessedLetters4",
+		message:"And The Last Guess"
+	}
+	]).then(function game (answer){
+		
+		initialWord.push(answer.guessedLetters1,answer.guessedLetters2,answer.guessedLetters3,answer.guessedLetters4);
+
+		// var finalWord = initialWord.concat(guessedLetters);
+
+		if (JSON.stringify(initialWord) == JSON.stringify(carname)){
+			console.log("\n" + "The Word is " + wordArray[7] + ", Good Job");
+			wins++;
+		} else {
+			console.log("\n" + "You Guessed Wrong" + "\n");
+			losses++;
+		}
+		
+		console.log("\n" + "You got " + wins + " out of 4 word(s) right");
+	})
+
+}
